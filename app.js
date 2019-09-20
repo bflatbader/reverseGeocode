d1bd18dc512fc6e62766f1544ec9694848230b5a
Bishop Bader <bflatbader@gmail.com>
2019-09-20 11:39:46 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// VARIABLES
+var latitude;
+var longitude;
+var cageDataAPIkey = '615d759215544dda9fb8fe2ed1fe1fff'
+
+// HTML 5 GeoLocation API
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        // Get latitude and longitude from browser
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        $('#lat').text(latitude);
+        $('#long').text(longitude);
+
+        // Define GeoNames query URL to reverse geocode 
+        var geoNamesURL = 'http://api.geonames.org/findNearbyPostalCodesJSON?lat=' + latitude + '&lng=' + longitude + '&username=bflatbader&maxRows=1'
+
+        // AJAX request
+        $.ajax({
+            url: geoNamesURL,
+            method: "GET"
+        })
+        // After data comes back from the request
+        .then(function(gnResults) {
+            console.log(gnResults);
+            var zip = gnResults.postalCodes[0].postalCode;
+            $('#zip').text(zip);
+        });
+    });
+}
